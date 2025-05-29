2d4ecc538de5b8a6e9d2b2b1cac3d324779ac849
JustynaNeuvonen <justyna.neuvonen@ometta.fi>
2025-05-29 13:25:22 +0300

    vaksynjoki translations

--- src/pages/vaaksynjoki/Vaaksynjoki.jsx
+++ src/pages/vaaksynjoki/Vaaksynjoki.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next';
+import './Vaaksynjoki.css';
+import vaaksy1 from '../../assets/Vaaksy1.jpg';
+import vaasky2 from '../../assets/Vaaksy2.jpg';
+import vaaksy3 from '../../assets/Vaaksy3.jpg';
+import vaaksy4 from '../../assets/Vaaksy4.jpg';
+import vaaksy5 from '../../assets/Vaaksy5.jpg';
 
 function Vaaksynjoki() {
+  const { t } = useTranslation();
   return (
     <div>
-      <h1>Vaaksynjoki</h1>  
-        <p>Vaaksynjoki is a river located in the municipality of Ruokolahti, Finland. It is known for its picturesque landscapes and is a popular spot for fishing and outdoor activities. The river flows through lush forests and offers opportunities for hiking and nature observation.</p>
+      <h1>{t('Vaaksyh1')}</h1>  
+      <h2>{t('Vaaksyh2')}</h2>
+      <p>{t('Vaaksyh3')}</p>
+      <p>{t ('Vaaksyp1')}</p>
+      <p>{t ('Vaaksyp2')}</p>
+      <p>{t ('Vaaksyp3')}</p>
+      <p>{t ('Vaaksyp4')}</p>
+
+
+    
     </div>
   )
 }
